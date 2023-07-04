a80e47ec3a695569430e8eeb308fc23a1b838446
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-07-04 17:15:44 +0500

    Feature: Added Change Theme Feature.

--- src/components/login-signup/Signup.js
+++ src/components/login-signup/Signup.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signup.css";
 
 const Signup = () => {
+  const [theme, setTheme] = useState(false);
 
+  const handleDark = () => {
+
+  
+    if(theme !== "dark-theme-component"){
+
+        setTheme("dark-theme-component");}
     
+   else{
+    setTheme("")
+   }
+      
 
-
+   
+  };
 
   return (
     <>
-      <div className="signup-parent">
+      <button onClick={handleDark} className="theme-btn-parent">
+        Change Theme <i class="bi bi-brightness-high"></i>
+      </button>
+      <div className={theme ? "dark-theme-component" : "signup-parent"}>
         <div className="headings">
           <span>
             <h2>SignUp</h2>
@@ -21,21 +36,20 @@ const Signup = () => {
         <div>
           <form>
             <div className="input input-email">
-              <input className="email"
-               type="email"
-               placeholder="Enter Email Here..."
-                />
+              <input
+                className="email"
+                type="email"
+                placeholder="Enter Email Here..."
+              />
             </div>
             <div className="input input-password">
-              <input className="password"
-               type="password"
-               placeholder="Enter Password Here..."
-                />
-                <button className="signup-btn">
-                SignUp
-            </button>
+              <input
+                className="password"
+                type="password"
+                placeholder="Enter Password Here..."
+              />
+              <button className="signup-btn">SignUp</button>
             </div>
-            
           </form>
         </div>
       </div>
